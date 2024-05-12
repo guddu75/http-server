ceb28a913645f7ad0590248294053dd3e17c2733
Debopriyo <guddumajumder75@gmail.com>
2024-05-12 20:28:07 +0530

    response with 200

--- app/server.go
+++ app/server.go
@@ -19,9 +19,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	con, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer con.Close()
+
+	con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
 }
