1b2ec4c93250ac016827024b952a80e2496e02f2
Debopriyo <guddumajumder75@gmail.com>
2024-05-14 20:30:29 +0530

    respond with 404 01

--- app/server.go
+++ app/server.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
+
+func response200(con net.Conn) {
+	con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+}
+
+func response404(con net.Conn) {
+	con.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+}
+
+func handleRequest(con net.Conn) {
+
+	defer con.Close()
+	buf := make([]byte, 1024)
+
+	contentLength, _ := con.Read(buf)
+
+	content := string(buf[:contentLength])
+
+	fmt.Println(content)
+
+	path := strings.Split(content, " ")[1]
+
+	if path == "/" {
+		response200(con)
+	} else {
+		response404(con)
+	}
+
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,21 +50,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	defer l.Close()
+
 	con, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	defer con.Close()
+	handleRequest(con)
 	//response
-
-	buf := make([]byte, 1024)
-
-	con.Read(buf)
-
-	fmt.Println(buf)
-
-	con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
 }
