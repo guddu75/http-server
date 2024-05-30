876a8406338123b3630412cb5585288cd00dd2aa
Debopriyo <guddumajumder75@gmail.com>
2024-05-30 23:51:11 +0530

    post a file 03

--- app/server.go
+++ app/server.go
@@ -10,18 +10,50 @@ import (
 var directory string
 
 type Request struct {
-	method  string
-	path    string
-	headers map[string]string
-	body    string
+	method      string
+	path        string
+	httpVersion string
+	host        string
+	headers     map[string]string
+	body        string
 }
 
-// func parseRe(con net.Conn) Request {
+func parseRequest(con net.Conn) *Request {
+	buf := make([]byte, 1024)
 
-// }
+	contentLength, _ := con.Read(buf)
 
-func responseEcho(con net.Conn, path string) {
-	msg := strings.Split(path, "/")[2]
+	content := string(buf[:contentLength])
+
+	req := new(Request)
+
+	fmt.Println("content here ", content)
+
+	lines := strings.Split(content, "\r\n")
+
+	firstLine := strings.Split(lines[0], " ")
+
+	req.method = firstLine[0]
+
+	req.path = firstLine[1]
+
+	req.httpVersion = firstLine[2]
+
+	req.host = strings.Split(lines[1], ": ")[1]
+
+	headers := strings.Split(lines[2], ": ")
+
+	for i := 0; i < len(headers); i += 2 {
+		req.headers[headers[i]] = headers[i+1]
+	}
+
+	req.body = lines[4]
+
+	return req
+}
+
+func responseEcho(con net.Conn, req Request) {
+	msg := strings.Split(req.path, "/")[2]
 	resp := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(msg)) + "\r\n\r\n" + msg
 	con.Write([]byte(resp))
 }
@@ -38,17 +70,17 @@ func response404(con net.Conn) {
 	con.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
 
-func responseUserAgent(con net.Conn, content string) {
-	lines := strings.Split(content, "\r\n")
-	fmt.Println("lines here", len(lines), lines)
+func responseUserAgent(con net.Conn, req Request) {
+	// lines := strings.Split(content, "\r\n")
+	// fmt.Println("lines here", len(lines), lines)
 
-	userAgent := strings.Split(lines[2], ": ")[1]
+	userAgent := req.headers["User-Agent"]
 	resp := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(userAgent)) + "\r\n\r\n" + userAgent
 	con.Write([]byte(resp))
 }
 
-func responseFile(con net.Conn, path string) {
-	filename := strings.Split(path, "/")[2]
+func responseFile(con net.Conn, req Request) {
+	filename := strings.Split(req.path, "/")[2]
 	filepath := fmt.Sprintf("%s/%s", directory, filename)
 	_, err := os.Stat(filepath)
 
@@ -77,31 +109,21 @@ func handleRequest(con net.Conn) {
 
 	defer con.Close()
 
-	buf := make([]byte, 1024)
+	req := parseRequest(con)
 
-	contentLength, _ := con.Read(buf)
-
-	content := string(buf[:contentLength])
-
-	fmt.Printf("content here %q", content)
-
-	path := strings.Split(content, " ")[1]
-
-	method := strings.Split(content, " ")[0]
-
-	if method == "POST" {
-		if strings.HasPrefix(path, "/files/") {
+	if req.method == "POST" {
+		if strings.HasPrefix(req.path, "/files/") {
 			// postFile(con, content)
 		}
-	} else {
-		if path == "/" {
+	} else if req.method == "GET" {
+		if req.path == "/" {
 			response200(con)
-		} else if path == "/user-agent" {
-			responseUserAgent(con, content)
-		} else if strings.HasPrefix(path, "/echo/") {
-			responseEcho(con, path)
-		} else if strings.HasPrefix(path, "/files/") {
-			responseFile(con, path)
+		} else if req.path == "/user-agent" {
+			responseUserAgent(con, *req)
+		} else if strings.HasPrefix(req.path, "/echo/") {
+			responseEcho(con, *req)
+		} else if strings.HasPrefix(req.path, "/files/") {
+			responseFile(con, *req)
 		} else {
 			response404(con)
 		}
